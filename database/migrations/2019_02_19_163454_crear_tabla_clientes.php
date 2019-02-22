4effe4162a015ff04d49138d1f18af60b722a4a0
root <scomacorcuera@iesesteveterradas.cat>
2019-02-22 16:33:15 +0100

    Tabla Clientes

---
+++ database/migrations/2019_02_19_163454_crear_tabla_clientes.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaClientes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+    Schema::dropIfExists('clients');
+
+    Schema::create('clients', function($table){
+        $table->increments('IdClient');
+        $table->string('Nombre');
+        $table->string('Email')->unique();
+        $table->integer('Telefono');
+        $table->string('Direccio');
+        $table->string('CIF/NIF')->unique();
+        $table->string('Provincia');
+        $table->string('Localidad');
+        $table->integer('Codigo Postal');
+        $table->timestamps();
+    });
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
