e5ce7f445437a0cf132ee1cab9ec6507f272f9a4
root <scomacorcuera@iesesteveterradas.cat>
2019-03-05 20:26:04 +0100

    Errores

--- public/js/componenteErrores.js
+++ public/js/componenteErrores.js
@@ -2,36 +2,47 @@ var logicaNombreProvinciaLocalidad = "^[a-z A-Z]{3,30}$";
 var logicaMail = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$";
 var logicaDireccion = "^[a-z A-Z,.]{5,100}$";
 var logicaTelefono = "^[0-9]{9}$";
+var contadorErrores = 0;
 // var logicaCodigoPostal = "^[0-9]{5}$";
 // var logicaNIFCIF = 
 
-function habilitarSubmit (idForm) {
-	$(idForm + " button.submit").removeAttr("disabled");
+window.onload=function(){
+	onClick("form");
+};
+function  onClick(idForm){
+	$(idForm + " #submit").click(function(){ checkForm(idForm); return false; });
 }
- 
-function deshabilitarSubmit (idForm) {
-	$(idForm + " button.submit").attr("disabled", "disabled");
+function añadirBorderError(idForm, idInput){	
+        $(idForm+ " " + idInput).addClass("border border-danger");
+	}
+function eliminarBorderError(idForm, idInput){	
+        $(idForm+ " " + idInput).removeClass("border border-danger");
+	}
+function comprobacion(idInput, logica, idForm) {
+	if( $(idInput).val().match(logica)){
+		eliminarBorderError(idForm, idInput);
+		return true;
+	}else{
+		mostrarErrores(idInput + " es erroneo.");
+		añadirBorderError(idForm, idInput);
+		contadorErrores++;
+		return false;
+	}
 }
+function mostrarErrores(idInput){
+	console.log(idInput);
 
-function comprobacion(idInput, logica) {
-	return $(idInput).val().match(logica) ? true : false;
 }
 
 function checkForm (idForm) {
-	$(idForm).on("change keydown", function() {
-		if (comprobacion("#nombre", logicaNombreProvinciaLocalidad)){
-
-			habilitarSubmit(idForm);
-		 
-			// && 
-			// comprobacion("#direccion", logicaDireccion) && 
-			// comprobacion("#telefono", logicaTelefono) && 
-			// comprobacion("#provincia", logicaNombreProvinciaLocalidad) && 
-			// comprobacion("#localidad", logicaNombreProvinciaLocalidad) && 
-			// comprobacion("#email", logicaMail))
-		
-		} else {
-			deshabilitarSubmit(idForm);
-			}
-		});
+	contadorErrores = 0;
+	comprobacion("#nombre", logicaNombreProvinciaLocalidad, idForm)
+	comprobacion("#provincia", logicaNombreProvinciaLocalidad, idForm) 
+	comprobacion("#localidad", logicaNombreProvinciaLocalidad, idForm)
+	comprobacion("#telefono", logicaTelefono, idForm)
+	comprobacion("#email", logicaMail, idForm)
+	if(contadorErrores === 0){
+		submit();
+	}
+			
 }
